361ff1256cce6c2c77246fe3178b745bdada9dc1
Zahramahjabinraisa <zahrafakrul@gmail.com>
2026-05-25 14:00:27 +0600

    Add files via upload

--- TokenType.java
+++ TokenType.java
@@ -1,17 +1,23 @@
 public enum TokenType {
-    // Keywords (Data Types)
+    // Keywords (Data Types & Control Flow)
     DATATYPE_INT,    // সংখ্যা
     DATATYPE_FLOAT,  // দশমিক
+    KEYWORD_IF,      // যদি
+    KEYWORD_ELSE,    // নাহলে
+    KEYWORD_WHILE,   // যতক্ষণ
 
     // Identifiers & Literals
     IDENTIFIER, NUMBER,
 
-    // Operators
+    // Operators & Logic
     ASSIGN,          // =
     PLUS, MINUS,     // + -
     MULTIPLY, DIVIDE,// * /
+    GREATER, LESS,   // > <
+    EQUAL_EQUAL,     // ==
 
     // Punctuation
     SEMICOLON,       // ;
+    LBRACE, RBRACE,  // { }
     EOF              // End of File
 }
